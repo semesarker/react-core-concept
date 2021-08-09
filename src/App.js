affe1df479b6d082d7e75403b2f56cb331765b4b
Seme Sarker <semesarker05@gmail.com>
2021-08-09 14:10:42 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // const nayok=['api','naki','saki']
+  const nayok=[{id:1,name:'nimi',age:23},
+               {id:2,name:'hini',age:56},
+              {id:3,name:'gubu',age:34}]
+              console.log(nayok);
+  
+  const [users,setUsers]=useState([])            
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+  },[])            
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* {nayok.map((person)=>{
+        return(<div>
+          <p>{person}</p>
+          </div>)
+      })} */}
+
+      {users.map((user)=>{
+        return(<>
+        <User user={user} />
+        </>)
+      })}
+
+      {nayok.map((person)=>{
+        const{id,name,age}=person;
+        const personStyle={
+          border:'2px solid red',
+          margin:'4px',
+          width:'200px'
+        }
+        return(
+        // <div key={id} style={personStyle}>
+        //   <h5>{name}</h5>
+        //   <p>{age}</p>
+        // </div>
+        <div>
+          {/* <Person key={person.id}{...person}/> */}
+          <Person person={person} />
+        </div>
+        )
+
+      })}
+        
       </header>
     </div>
   );
+}
+
+function User(props){
+  console.log(props)
+  return(<>
+   <div key={props.user.id}>
+     <p>{props.user.name}</p>
+   </div>
+  </>)
+}
+
+function Person(props){
+  // console.log(props);
+   const personStyle={
+          border:'2px solid red',
+          margin:'4px',
+          width:'200px'
+        }
+  return (<>
+  <div style={personStyle}>
+    <h5>{props.person.name}</h5>
+    <p>{props.person.age}</p>
+  </div>
+  </>) 
 }
 
 export default App;
